018d7b9a3552e3a2cd25542be602ff79b17801f8
niranjangirhe <niranjangirheindia@gmail.com>
2021-12-29 01:53:39 +0530

    done required changes

--- index.js
+++ index.js
@@ -65,3 +65,7 @@ function addinputfield(){
              <input class="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 focus:bg-transparent text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" placeholder="New Entry" type="text" name="">
             </p>`
 }
+function addbtnpressed()
+{
+  console.log("added")
+}
