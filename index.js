dc5f366cce8daf86c86b348a058b941914911598
niranjangirhe <niranjangirheindia@gmail.com>
2022-04-18 04:03:53 +0530

    Added Edit FE

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+
 var db = null;
 var index = -1;
 var users = [];
 var myemail = "";
 var report = [];
+var psuedostack = [];
+
 window.addEventListener("DOMContentLoaded", function () {
   db = firebase.firestore();
   readentry()
@@ -22,7 +25,9 @@ window.addEventListener("DOMContentLoaded", function () {
   datesrtring = datenum + " " + months[month] + " " + year + " " + hr + ":" + min;
   date.innerHTML = datesrtring
 }, false);
-function addcardcardwrapper(name, list, price, description, date) {
+
+
+function addcardcardwrapper(name, list, price, description, date,docid, index) {
   var newdate = new Date(date * 1000)
   var datenum = newdate.getDate()
   var month = newdate.getMonth()
@@ -53,7 +58,7 @@ function addcardcardwrapper(name, list, price, description, date) {
       <h2 class="text-sm tracking-widest text-gray-400 title-font mb-1 font-medium">`+ name + `</h2>
       <h1 class="text-5xl text-white pb-4 mb-4 border-b border-gray-800 leading-none">`+ "₹ " + price + `</h1>` +
     liststring
-    + `<button class="flex items-center mt-auto text-white bg-green-500 border-0 py-2 px-4 w-full focus:outline-none hover:bg-green-600 rounded">Button
+    + `<button class="flex items-center mt-auto text-white bg-green-500 border-0 py-2 px-4 w-full focus:outline-none hover:bg-green-600 rounded" onclick="EditEntry('` + index +`','`+ docid + `')">Edit
             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-auto" viewBox="0 0 24 24">
               <path d="M5 12h14M12 5l7 7-7 7"></path>
             </svg>
@@ -76,7 +81,7 @@ function addentry(name, list, price, description) {
   }
 
 
-  // To update age and favorite color:
+
   db.collection("group").doc(localStorage.getItem("grouplink")).update({
     report: report
   })
@@ -92,24 +97,24 @@ function addentry(name, list, price, description) {
   })
     .then((docRef) => {
       console.log("Document written with ID: ", docRef.id);
+      addcardcardwrapper(name, list, price, description, Math.floor(Date.now() / 1000), docRef.id, psuedostack.length)
+      psuedostack.push([name, list, price, description, Math.floor(Date.now() / 1000), docRef.id, psuedostack.length]);
+      console.log(psuedostack);
     })
     .catch((error) => {
       console.error("Error adding document: ", error);
     });
 }
 function readentry() {
-
-  console.log(localStorage.getItem("grouplink"))
-
-  var psuedostack = []
   db.collection("group").doc(localStorage.getItem("grouplink")).collection("transaction").orderBy("date", "desc")
     .get()
     .then((querySnapshot) => {
       querySnapshot.forEach((doc) => {
-        psuedostack.push([doc.data().name, doc.data().list, doc.data().price, doc.data().description, doc.data().date])
+        psuedostack.push([doc.data().name, doc.data().list, doc.data().price, doc.data().description, doc.data().date, doc.id,psuedostack.length])
       });
+      console.log(psuedostack);
       for (var i = psuedostack.length - 1; i >= 0; i--) {
-        addcardcardwrapper(psuedostack[i][0], psuedostack[i][1], psuedostack[i][2], psuedostack[i][3], psuedostack[i][4])
+        addcardcardwrapper(psuedostack[i][0], psuedostack[i][1], psuedostack[i][2], psuedostack[i][3], psuedostack[i][4], psuedostack[i][5], psuedostack[i][6])
       }
     })
     .catch((error) => {
@@ -117,9 +122,6 @@ function readentry() {
     });
 }
 
-function readentryonces(name, list, amount, discription) {
-  addcardcardwrapper(name, list, amount, discription, Math.floor(Date.now() / 1000))
-}
 
 //ui
 function addinputfield() {
@@ -140,6 +142,29 @@ function addinputfield() {
     entry[i].value = list[i];
   }
 }
+
+function addinputfieldmodal() {
+  var list = []
+  var entry = document.getElementsByClassName("entrymodal");
+  for (var i = 0; i < entry.length; i++) {
+    console.log(entry[i].value.trim())
+    list.push(entry[i].value.trim());
+  }
+
+  var inputF = document.getElementById('inputfieldmodal')
+  inputF.innerHTML += `<p class="flex items-center text-gray-400 mb-2" >
+  <input
+    class="entrymodal w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 focus:bg-transparent text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+    placeholder="New Entry" type="text" name="" id="entrymodal">
+</p>`
+
+  for (var i = 0; i < entry.length - 1; i++) {
+    entry[i].value = list[i];
+  }
+}
+
+
+
 function addbtnpressed() {
   var name = document.getElementById("name").value.trim()
   var amount = document.getElementById("amount").value
@@ -153,10 +178,8 @@ function addbtnpressed() {
       if (entry[i].value.trim() != "")
         list.push(entry[i].value.trim());
     }
-    console.log(list)
     addentry(firebase.auth().currentUser.email, list, amount, discription)
     clearinputfeilds()
-    readentryonces(name, list, amount, discription)
   }
 }
 function clearinputfeilds() {
@@ -175,24 +198,29 @@ function clearinputfeilds() {
 }
 
 window.addEventListener('DOMContentLoaded', (event) => {
+  //var label = document.getElementById("label");
+  
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
       db.collection("group").doc(localStorage.getItem("grouplink")).get().then((doc) => {
         if (doc.exists) {
+          
           console.log(doc.data().name);
           console.log(doc.id);
           users = doc.data().list;
           report = doc.data().report;
+      
+          var label = document.getElementById("label");
           for (var i = 0; i < users.length; i++) {
+      
             if (users[i] == user.email) {
               index = i;
-              console.log(index);
             }
-            console.log(users[i])
-            label.innerHTML += ` <label class="inline-flex items-center mt-3">
-          <input type="checkbox" class="form-checkbox h-5 w-5 text-gray-400" checked><span class="ml-2 text-gray-400">`+ users[i] + `</span>
-      </label>`
+            label.innerHTML += `<label class="inline-flex items-center mt-3">
+            <input type="checkbox" class="form-checkbox h-5 w-5 text-gray-400" checked><span class="ml-2 text-gray-400">`+ users[i] + `</span>
+        </label>`
           }
+          document.getElementById("labelmodal").innerHTML=label.innerHTML;
         } else {
           // doc.data() will be undefined in this case
           console.log("No such document!");
@@ -207,3 +235,17 @@ window.addEventListener('DOMContentLoaded', (event) => {
     }
   });
 });
+
+function EditEntry(index,id) {
+  console.log(index)
+  console.log(id)
+  var amountmodal = document.getElementById("amountmodal");
+  var discriptionmodal = document.getElementById("discriptionmodal");
+  var entrymodal = document.getElementById("entrymodal");
+  var labelmodal = document.getElementById("labelmodal");
+  var inputfieldmodal = document.getElementById("inputfieldmodal");
+  amountmodal.value = psuedostack[index][2];
+  discriptionmodal.value = psuedostack[index][3];
+  entrymodal.value  = psuedostack[index][1][0];
+  openModal()
+}
