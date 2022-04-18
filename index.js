765ad5a0b09e92fe56cb61a91a8c749c10608305
niranjangirhe <niranjangirheindia@gmail.com>
2022-04-19 03:41:56 +0530

    edit done

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ var username = [];
 var myemail = "";
 var report = [];
 var psuedostack = [];
-
+var editDocid;
+var oldPrice;
+var oldUser = [];
+var oldindex;
 window.addEventListener("DOMContentLoaded", function () {
   db = firebase.firestore();
 
@@ -72,6 +75,13 @@ function addcardcardwrapper(name, list, price, description, date, docid, index, 
     </span>`+ username[i] +
         `</p>`
   }
+  var buttoncode = ""
+  if (name == firebase.auth().currentUser.displayName)
+    buttoncode = `<button class="flex items-center mt-auto text-white bg-green-500 border-0 py-2 px-4 w-full focus:outline-none hover:bg-green-600 rounded" onclick="EditEntry('` + index + `','` + docid + `')">Edit
+  <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-auto" viewBox="0 0 24 24">
+    <path d="M5 12h14M12 5l7 7-7 7"></path>
+  </svg>
+</button>`
   datesrtring = datenum + " " + months[month] + " " + year + " " + hr + ":" + min;
   t.innerHTML = `<div class="p-4 xl:w-1/4 md:w-1/2 w-full">
     <div class="h-full p-6 rounded-lg border-2 border-green-500 flex flex-col relative overflow-hidden">
@@ -79,11 +89,7 @@ function addcardcardwrapper(name, list, price, description, date, docid, index, 
       <h2 class="text-sm tracking-widest text-gray-400 title-font mb-1 font-medium">`+ name + `</h2>
       <h1 class="text-5xl text-white pb-4 mb-4 border-b border-gray-800 leading-none">`+ "₹ " + price + `</h1>` +
     liststring
-    + `<button class="flex items-center mt-auto text-white bg-green-500 border-0 py-2 px-4 w-full focus:outline-none hover:bg-green-600 rounded" onclick="EditEntry('` + index + `','` + docid + `')">Edit
-            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-auto" viewBox="0 0 24 24">
-              <path d="M5 12h14M12 5l7 7-7 7"></path>
-            </svg>
-          </button>
+    + buttoncode + `
       <p class="text-xs text-gray-400 mt-3">`+ description + `</p>
     </div>
   </div>`+ t.innerHTML;
@@ -92,23 +98,37 @@ function addcardcardwrapper(name, list, price, description, date, docid, index, 
 //firebase
 function addentry(name, list, price, description) {
 
-  for (var i = 0; i < report.length; i++) {
-    if (i == index) {
-      report[i] += (price * (report.length - 1)) / report.length;
-    }
-    else {
-      report[i] -= price / report.length;
-    }
-  }
+
+
 
   var userlist = [];
   var userdoc = document.getElementsByClassName("fc");
+  var countuser = 0;
   for (var i = 0; i < userdoc.length; i++) {
     userlist.push(userdoc[i].checked)
+    if (userdoc[i].checked)
+      countuser++;
   }
 
-  console.log(userlist);
+  for (var i = 0; i < report.length; i++) {
 
+    if (i == index) {
+      if (userdoc[i].checked) {
+        report[i] += (price * (countuser - 1)) / countuser;
+      }
+      else {
+        report[i] += (price * (countuser)) / countuser;
+      }
+    }
+    else {
+      if (userdoc[i].checked) {
+        report[i] -= price / countuser;
+      }
+    }
+
+  }
+  console.log(userlist);
+  console.log(report);
   db.collection("group").doc(localStorage.getItem("grouplink")).update({
     report: report
   })
@@ -194,7 +214,6 @@ function addinputfieldmodal() {
 
 
 function addbtnpressed() {
-  var name = document.getElementById("name").value.trim()
   var amount = document.getElementById("amount").value
   var discription = document.getElementById("discription").value
   var entry = document.getElementsByClassName("entry");
@@ -237,7 +256,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
           console.log(doc.id);
           users = doc.data().list;
           report = doc.data().report;
-
+          console.log(report);
           var label = document.getElementById("label");
           var labelmodal = document.getElementById("labelmodal");
           for (var i = 0; i < users.length; i++) {
@@ -263,6 +282,9 @@ window.addEventListener('DOMContentLoaded', (event) => {
 });
 
 function EditEntry(index, id) {
+  console.log(report);
+  editDocid = id;
+  oldindex = index;
   console.log(index)
   console.log(id)
   var amountmodal = document.getElementById("amountmodal");
@@ -273,21 +295,20 @@ function EditEntry(index, id) {
   amountmodal.value = psuedostack[index][2];
   discriptionmodal.value = psuedostack[index][3];
   var inputF = document.getElementById('inputfieldmodal')
-  inputF.innerHTML="";  
-  for(var i=0;i<psuedostack[index][1].length;i++){
+  inputF.innerHTML = "";
+  for (var i = 0; i < psuedostack[index][1].length; i++) {
     inputF.innerHTML += `<p class="flex items-center text-gray-400 mb-2" >
   <input
     class="entrymodal w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 focus:bg-transparent text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-    placeholder="New Entry" type="text" name="" id="entrymodal" value="`+psuedostack[index][1][i]+`">
+    placeholder="New Entry" type="text" name="" id="entrymodal" value="`+ psuedostack[index][1][i] + `">
 </p>`
   }
-  for(var i=0;i<mod.length;i++){
-    if(psuedostack[index][7][i])
-    {
-      mod[i].checked=true;
+  for (var i = 0; i < mod.length; i++) {
+    if (psuedostack[index][7][i]) {
+      mod[i].checked = true;
     }
-    else{
-      mod[i].checked=false;
+    else {
+      mod[i].checked = false;
     }
   }
   openModal()
@@ -316,4 +337,109 @@ async function checklistfiller() {
     });
   }
   readentry()
+}
+function editEntryDB() {
+  var amount = document.getElementById("amountmodal").value
+  var discription = document.getElementById("discriptionmodal").value
+  var entry = document.getElementsByClassName("entrymodal");
+  if (amount == "" || entry[0].value.trim() == "")
+    alert("Please Enter complete Info")
+  else {
+    var list = [];
+    for (var i = 0; i < entry.length; i++) {
+      if (entry[i].value.trim() != "")
+        list.push(entry[i].value.trim());
+    }
+    updateentry(firebase.auth().currentUser.displayName, list, amount, discription)
+    clearinputfeilds()
+  }
+}
+
+
+function updateentry(name, list, price, description) {
+  console.log("old : " + psuedostack[oldindex])
+  console.log("new : " + name + " " + list + " " + price + " " + description)
+  console.log(report);
+  var userlist = [];
+  var userdoc = document.getElementsByClassName("mod");
+  var countuser = 0;
+  var oldcountuser = 0;
+  for (var i = 0; i < userdoc.length; i++) {
+    userlist.push(userdoc[i].checked)
+    if (userdoc[i].checked)
+      countuser++;
+  }
+
+  for (var i = 0; i < psuedostack[oldindex][7].length; i++) {
+    if (psuedostack[oldindex][7][i])
+      oldcountuser++;
+  }
+
+  console.log("old : " + oldcountuser)
+  var userchange = false;
+  for (var i = 0; i < psuedostack[oldindex][7].length; i++) {
+    if (psuedostack[oldindex][7][i] != userlist[i]) {
+      userchange = true;
+    }
+  }
+  console.log("olduser : " + psuedostack[oldindex][7])
+  if (psuedostack[oldindex][2] != price || userchange) {
+    for (var i = 0; i < report.length; i++) {
+
+      if (i == index) {
+        if ( psuedostack[oldindex][7][i]) {
+          report[i] -= (psuedostack[oldindex][2]* (oldcountuser - 1)) / oldcountuser;
+        }
+        else {
+          report[i] -= (psuedostack[oldindex][2] * (oldcountuser)) / oldcountuser;
+        }
+      }
+      else {
+        if (psuedostack[oldindex][7][i]) {
+          report[i] += psuedostack[oldindex][2] / oldcountuser;
+        }
+      }
+    }
+
+    for (var i = 0; i < report.length; i++) {
+
+      if (i == index) {
+        if (userdoc[i].checked) {
+          report[i] += (price * (countuser - 1)) / countuser;
+        }
+        else {
+          report[i] += (price * (countuser)) / countuser;
+        }
+      }
+      else {
+        if (userdoc[i].checked) {
+          report[i] -= price / countuser;
+        }
+      }
+  
+    }
+  }
+
+  console.log(report);
+
+  db.collection("group").doc(localStorage.getItem("grouplink")).update({
+    report: report
+  })
+    .then(() => {
+      console.log("Document successfully updated!");
+    });
+
+    db.collection("group").doc(localStorage.getItem("grouplink")).collection("transaction").doc(editDocid).update({
+      name: name,
+      list: list,
+      price: price,
+      description: description,
+      userlist: userlist,
+      date: psuedostack[oldindex][4]
+  })
+  .then(() => {
+      console.log("Document successfully updated!");
+      window.location.reload();
+  });
+  
 }
